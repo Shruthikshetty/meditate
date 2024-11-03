11ca69ab23e122229fe8463ad0fbdd44259b7a4e
shruthik shetty <shrutikshetty14@gmail.com>
2024-11-03 13:38:10 +0530

    Add native wind setup + const files

--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,10 @@
+// Import your global CSS file
+import "../global.css";
 import { StyleSheet, View, Text } from "react-native";
 
 export default function HomeScreen() {
   return (
-    <View style={styles.container}>
+    <View className="flex-1 justify-center items-center">
       <Text>Hello world</Text>
     </View>
   );
